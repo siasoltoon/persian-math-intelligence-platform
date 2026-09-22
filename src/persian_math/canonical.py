5b1665d51f677433000849e3e5199c49639cfad2
siasoltoon <siyavashjahangiri105@gmail.com>
2026-09-22 11:44:40 +0330

    feat(canonical): broaden safe real and complex math primitives

--- src/persian_math/canonical.py
+++ src/persian_math/canonical.py
@@ -18,6 +18,15 @@ _TRANSFORMATIONS = standard_transformations + (implicit_multiplication_applicati
 _LOCALS = {
     "pi": sp.pi,
     "e": sp.E,
+    "oo": sp.oo,
+    "I": sp.I,
+    "re": sp.re,
+    "im": sp.im,
+    "conjugate": sp.conjugate,
+    "arg": sp.arg,
+    "sign": sp.sign,
+    "Min": sp.Min,
+    "Max": sp.Max,
     "sqrt": sp.sqrt,
     "sin": sp.sin,
     "cos": sp.cos,
@@ -53,6 +62,8 @@ _SAFE_GLOBALS = {
     "Function": sp.Function,
     "pi": sp.pi,
     "E": sp.E,
+    "oo": sp.oo,
+    "I": sp.I,
 }
 _MAX_MATH_INPUT_CHARS = 10_000
 _ALLOWED_MATH_CHARS = re.compile(r"^[A-Za-z0-9_+\-*/^().,\[\]{}<>=|:;\s]*$")
